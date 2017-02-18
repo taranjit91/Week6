6de72bc04ba134325f2e66aeeb13137594632d2f
taranjit91 <parmar.taran@gmail.com>
2017-02-18 09:10:53 -0500

    first commit

---
+++ server/app.js
@@ -0,0 +1,81 @@
+let express = require('express');
+let path = require('path'); // part of node.js core
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+
+// import "mongoose"
+let mongoose = require('mongoose');
+
+// URI
+let config = require('./config/db');
+
+mongoose.connect(process.env.URI || config.URI);
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log("Conneced to MongoDB...");
+});
+
+// define routers
+let index = require('./routes/index'); // top level routes
+let games = require('./routes/games'); // routes for games
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../client')));
+
+// route redirects
+app.use('/', index);
+app.use('/games', games);
+
+// Handle 404 Errors
+  app.use(function(req, res) {
+      res.status(400);
+     res.render('errors/404',{
+      title: '404: File Not Found'
+    });
+  });
+
+  // Handle 500 Errors
+  app.use(function(error, req, res, next) {
+      res.status(500);
+      res.render('errors/500', {
+        title:'500: Internal Server Error',
+        error: error
+      });
+  });
+
+/*
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) =>{
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+*/
+
+module.exports = app;
---
+++ server/routes/index.js
@@ -0,0 +1,26 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+let game = require('../models/games');
+
+/* GET home page. wildcard */
+router.get('/', (req, res, next) => {
+  res.render('content/index', {
+    title: 'Home',
+    games: ''
+   });
+});
+
+
+
+
+/* GET contact page. */
+router.get('/contact', (req, res, next) => {
+  res.render('content/contact', {
+    title: 'Contact',
+    games: ''
+   });
+});
+
+module.exports = router;
